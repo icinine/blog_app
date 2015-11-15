79939f3e0d2d444d91333047c37c0d6b5611e206
icinine <shane.carr@sitesignals.com>
2015-11-15 16:58:47 +0000

    link articles to users

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Creating Articles" do
   
+  before do
+    @john = User.create!(email: "john@example.com", password: "password")
+    login_as(@john)
+  end
+  
+  
   
   scenario "A user creates a new article" do
     visit "/"
@@ -14,6 +20,7 @@ RSpec.feature "Creating Articles" do
     
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
+    expect(page).to have_content("Created by: #{@john.email}")
   end
   
   
--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "editing an article" do
   
   before do
-    @article = Article.create(title:"First Article", body: "body of first article")
+    john = User.create!(email: "john@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title:"First Article", body: "body of first article", user: john)
   end
   
   scenario "A User updates an article" do
