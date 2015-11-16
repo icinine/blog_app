63e2e37d7d8d5f79d60128658d790b1d119bfdf0
icinine <shane.carr@sitesignals.com>
2015-11-16 20:23:25 +0000

    controller article edit access contols

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,17 +22,24 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    
+    if @article.user != current_user
+      flash[:danger] = "you can only edit your own article"
+      redirect_to root_path
+    end
   end
   
   def update
-    
-    if @article.update(article_params)
-      flash[:success] = "Article has been updated"
-      redirect_to @article
-    else
-      flash.now[:danger] = "Article has not been updated"
-      render :edit
+      if @article.user != current_user
+        flash[:danger] = "you can only edit your own article"
+        redirect_to root_path
+      else
+      if @article.update(article_params)
+        flash[:success] = "Article has been updated"
+        redirect_to @article
+      else
+        flash.now[:danger] = "Article has not been updated"
+        render :edit
+      end
     end    
   end
   
