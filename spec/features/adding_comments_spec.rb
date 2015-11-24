b7928d9ac1b9485370b7d4a71b0975d5bdd86fe5
icinine <shane.carr@sitesignals.com>
2015-11-24 22:51:33 +0000

    implemented further adding comments

--- spec/features/adding_comments_spec.rb
+++ spec/features/adding_comments_spec.rb
@@ -6,18 +6,18 @@ RSpec.feature "Adding Reviews to Articles" do
     @john = User.create(email:"john@example.com", password:"password")  
     @fred = User.create(email:"fred@example.com", password:"password")  
     
-    @article = Article.create(title: "Article Title", body:"Article Body", user:@john)
+    @article = Article.create(title: "Article Title", body:"Article Body", user: @john)
   end
   
   scenario "permit signed in user to write a review" do
-    login_as (@fred)
+    login_as(@fred)
     
     visit "/"
     click_link @article.title
     fill_in "New Comment", with: "Great article"
     click_button "Add Comment"
     
-    expect(page).to have_content("Article has been created")
+    expect(page).to have_content("Comment has been created")
     expect(page).to have_content("Great article")
     expect(current_path).to eq(article_path(@article.comments.last.id))
     
